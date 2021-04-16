d1b4b77939afd2fb5f8328109d93258f9e66f5aa
Mane Kirakosyan <kirakosyanmane03@gmail.com>
2021-04-16 20:39:33 +0400

    add chat and all files

---
+++ js/chat.js
@@ -0,0 +1,23 @@
+// const borderSize = 3;
+// const panel = document.getElementById("left__panel");
+//
+// let panelWidth;
+// function resize(e) {
+//     const dx = panelWidth - e.x;
+//     panelWidth = e.x;
+//     panel.style.width = (parseInt(getComputedStyle(panel, '').width) - dx) + "px";
+// }
+//
+//
+// panel.addEventListener("mousedown", (e) => {
+//     if (e.offsetX > borderSize) {
+//         panelWidth = e.x;
+//         document.addEventListener("mousemove", resize);
+//     }
+// });
+//
+//
+// document.addEventListener("mouseup", () => {
+//     document.removeEventListener("mousemove", resize);
+// });
+
