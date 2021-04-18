9e2f322dfad3180e8282647e59b476c13f3a32a0
Mane Kirakosyan <kirakosyanmane03@gmail.com>
2021-04-18 23:31:43 +0400

    add some functionality and new icons

--- js/chat.js
+++ js/chat.js
@@ -21,3 +21,25 @@
 //     document.removeEventListener("mousemove", resize);
 // });
 
+
+
+const conversations = document.querySelectorAll(".project__desc");
+
+for (let i = 0; i < conversations.length; i++) {
+  let totalHeight = 0
+  conversations[i].addEventListener("click", function () {
+    let result = this.nextElementSibling;
+    let activeSibling = this.nextElementSibling.classList.contains("active")
+    this.classList.toggle("arrow")
+    result.classList.toggle("active")
+    if(!activeSibling) {
+      for (let i = 0; i < result.children.length; i++) {
+        totalHeight += result.children[i].scrollHeight + 40
+      }
+    } else  {
+      totalHeight = 0
+    }
+    result.style.maxHeight = totalHeight + "px"
+  })
+}
+
